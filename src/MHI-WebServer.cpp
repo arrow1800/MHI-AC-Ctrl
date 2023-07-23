77409a4b64917f31aae94ffe5fbc1238a8767f4c
SmOeL-swdev <emielkosse@gmail.com>
2023-07-23 12:28:41 +0200

    Code clean-up firmware webserver

--- src/MHI-WebServer.cpp
+++ src/MHI-WebServer.cpp
@@ -10,21 +10,17 @@
 #include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
-/*#include <ArduinoJson.h>
-#include <ESP8266FtpServer.h>
-#include <LITTLEFS.h>*/
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
-//FtpServer ftpSrv;
+
 
 void WServer::Init(void){
-   //ftpSrv.begin(ftp_username,ftp_password);    //username, password for ftp.  set ports in ESP8266FtpServer.h  (default 21, 50009 for PASV)
+
     httpUpdater.setup(&httpServer, UPDATE_PATH, FW_USER, FW_PASSWORD);
     httpServer.begin();
 }
 
 void WServer::Handle(void){    
-   //ftpSrv.handleFTP();
    httpServer.handleClient();
 }
